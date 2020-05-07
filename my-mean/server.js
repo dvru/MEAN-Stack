d11da4ac667120589fa120a399374f4adba406aa
dvru <damvaru4@gmail.com>
2020-05-07 16:13:44 -0500

    added server

--- my-mean/server.js
+++ my-mean/server.js
@@ -1,9 +1,10 @@
 const http = require('http');
+const app = require('./backend/app')
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.end('This is my First Response');
-});
+app.set('port', port);
+const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+server.listen(port);
 
 
