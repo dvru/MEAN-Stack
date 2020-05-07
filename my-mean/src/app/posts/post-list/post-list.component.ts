5ce1fbf42f7eca7da8bbe2471267db662faa5785
dvru <damvaru4@gmail.com>
2020-05-06 19:01:05 -0500

    added posts

--- my-mean/src/app/posts/post-list/post-list.component.ts
+++ my-mean/src/app/posts/post-list/post-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
@@ -8,11 +9,19 @@ import { Post } from '../post.model';
   styleUrls: ['./post-list.component.css']
 })
 
-export class PostListComponent{
+export class PostListComponent implements OnInit {
   // posts = [
   //   {title: 'First Post', content: 'This is the first post\'s content'},
   //   {title: 'Second Post', content: 'This is the second post\'s content'},
   //   {title: 'Third Post', content: 'This is the third post\'s content'},
   // ];
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
+
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit(){
+    this.posts = this.postsService.getPosts();
+  }
 }
+
