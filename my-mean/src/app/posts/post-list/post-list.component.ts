ede196d61d1ec0d93703194fcd6fa7208b67d345
dvru <damvaru4@gmail.com>
2020-05-06 19:08:22 -0500

    added posts listener

--- my-mean/src/app/posts/post-list/post-list.component.ts
+++ my-mean/src/app/posts/post-list/post-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
 
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
@@ -9,23 +11,28 @@ import { PostsService } from '../posts.service';
   styleUrls: ['./post-list.component.css']
 })
 
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   //   {title: 'First Post', content: 'This is the first post\'s content'},
   //   {title: 'Second Post', content: 'This is the second post\'s content'},
   //   {title: 'Third Post', content: 'This is the third post\'s content'},
   // ];
   posts: Post[] = [];
+  private postsSub: Subscription
 
 
   constructor(public postsService: PostsService) {}
 
   ngOnInit(){
     this.posts = this.postsService.getPosts();
-    this.postsService.getPostUpdatedListener()
+    this.postsSub = this.postsService.getPostUpdatedListener()
       .subscribe((posts: Post[]) => {
         this.posts = posts;
       });
+  }
+
+  ngOnDestroy(){
+    this.postsSub.unsubscribe();
   }
 }
 
