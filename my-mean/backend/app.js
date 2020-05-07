71db4b37d646650bbe5a3600ff4c36a939574b71
dvru <damvaru4@gmail.com>
2020-05-07 17:02:37 -0500

    body-parser

--- my-mean/backend/app.js
+++ my-mean/backend/app.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
 
-app.use('/api/posts', (req, res, next) => {
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+  "Access-Control-Allow-Headers",
+  "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+  "Access-Control-Allow-Methods",
+  "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+})
+
+app.post("/api/posts", (req, res, next) => {
+  console.log();
+});
+
+app.use("/api/posts", (req, res, next) => {
     const posts = [
       { id: 'sfsffd',
         title: 'First server side post',
