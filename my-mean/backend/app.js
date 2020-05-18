c61133a5074d0cb1b6b5fbf957f599accf0cdd15
dvru <damvaru4@gmail.com>
2020-05-18 12:53:29 -0500

    can upload images

--- my-mean/backend/app.js
+++ my-mean/backend/app.js
@@ -18,7 +18,7 @@ mongoose.connect("mongodb+srv://damini:NHV0TxJsTt9QC8xu@cluster0-86cg6.mongodb.n
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
-app.use("/images", express.static());
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
