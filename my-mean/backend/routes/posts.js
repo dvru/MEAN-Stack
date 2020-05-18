b6dde178adaea1ec166d42d0f6fe0f8590e5f265
dvru <damvaru4@gmail.com>
2020-05-18 13:46:24 -0500

    adding page query

--- my-mean/backend/routes/posts.js
+++ my-mean/backend/routes/posts.js
@@ -72,7 +72,8 @@ router.get("", (req, res, next) => {
     const postQuery = Post.find();
     if (pageSize && currentPage) {
       postQuery
-      .skip(pageSize * (currentPage + 1));
+      .skip(pageSize * (currentPage - 1))
+      .limit(pageSize);
     }
     postQuery.then(documents => {
       res.status(200).json({
