5ed9a568c6f01591274415ae6b7328b98b5598cf
dvru <damvaru4@gmail.com>
2020-05-18 13:38:09 -0500

    adding paginator

--- my-mean/src/app/posts/post-list/post-list.component.ts
+++ my-mean/src/app/posts/post-list/post-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-
+import { Subscription } from 'rxjs';
+import { PageEvent } from '@angular/material/paginator';
 
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
-import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-post-list',
@@ -19,6 +20,9 @@ export class PostListComponent implements OnInit, OnDestroy {
   // ];
   posts: Post[] = [];
   isLoading = false;
+  totalPosts = 10;
+  postsPerPage = 2;
+  pageSizeOptions = [1, 2, 5, 10];
   private postsSub: Subscription
 
 
@@ -33,6 +37,10 @@ export class PostListComponent implements OnInit, OnDestroy {
         this.posts = posts;
 
       });
+  }
+
+  onChangePage(pageData: PageEvent) {
+    console.log(pageData);
   }
 
   onDelete(postId: string) {
