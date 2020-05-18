f455835e298031c901c927e11e87ff72a8afcfc5
dvru <damvaru4@gmail.com>
2020-05-18 13:16:40 -0500

    successfully updating images

--- my-mean/src/app/posts/posts.service.ts
+++ my-mean/src/app/posts/posts.service.ts
@@ -41,7 +41,7 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return this.http.get<{_id: string, title: string, content: string}>(
+    return this.http.get<{_id: string, title: string, content: string, imagePath: string}>(
       "http://localhost:3000/api/posts/" + id
       );
   }
@@ -73,6 +73,7 @@ export class PostsService {
       let postData: Post | FormData;
       if (typeof(image) === 'object') {
         postData = new FormData();
+        postData.append("id", id);
         postData.append("title", title);
         postData.append("content", content);
         postData.append("image", image, title);
@@ -92,7 +93,7 @@ export class PostsService {
           id: id,
           title: title,
           content: content,
-          imagePath: response.imagePath
+          imagePath: ""
       };
         updatedPosts[oldPostsIndex] = post;
         this.posts = updatedPosts;
