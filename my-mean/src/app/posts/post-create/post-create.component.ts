e1568c9d128cbc3f29ffeef315570327876f2bef
dvru <damvaru4@gmail.com>
2020-05-18 13:02:45 -0500

    updating images

--- my-mean/src/app/posts/post-create/post-create.component.ts
+++ my-mean/src/app/posts/post-create/post-create.component.ts
@@ -52,7 +52,8 @@ export class PostCreateComponent implements OnInit{
           };
           this.form.setValue({
             'title': this.post.title,
-            'content': this.post.content
+            'content': this.post.content,
+            'image': this.post.imagePath
           });
         });
       } else {
@@ -88,7 +89,8 @@ export class PostCreateComponent implements OnInit{
       this.postsService.updatePost(
         this.postId,
         this.form.value.title,
-        this.form.value.content
+        this.form.value.content,
+        this.form.value.image
         );
     }
     this.form.reset();
--- my-mean/src/app/posts/posts.service.ts
+++ my-mean/src/app/posts/posts.service.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs';
 import { Post } from './post.model';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { Form } from '@angular/forms';
 
 
 
@@ -68,12 +69,31 @@ export class PostsService {
       })
     }
 
-    updatePost(id: string, title: string, content: string){
-      const post: Post = { id: id, title: title, content: content, imagePath: null};
-      this.http.put('http://localhost:3000/api/posts/' + id, post)
+    updatePost(id: string, title: string, content: string, image: File | string ){
+      let postData: Post | FormData;
+      if (typeof(image) === 'object') {
+        postData = new FormData();
+        postData.append("title", title);
+        postData.append("content", content);
+        postData.append("image", image, title);
+      } else {
+        postData = {
+          id: id,
+          title: title,
+          content: content,
+          imagePath: image}
+      }
+
+      this.http.put('http://localhost:3000/api/posts/' + id, postData)
       .subscribe(response => {
         const updatedPosts = [...this.posts];
-        const oldPostsIndex = updatedPosts.findIndex(p => p.id === post.id);
+        const oldPostsIndex = updatedPosts.findIndex(p => p.id === id);
+        const post: Post = {
+          id: id,
+          title: title,
+          content: content,
+          imagePath: response.imagePath
+      };
         updatedPosts[oldPostsIndex] = post;
         this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
