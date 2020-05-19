1992e3888a6d3561367b0db5989dcad60af849c5
dvru <damvaru4@gmail.com>
2020-05-18 21:57:16 -0500

    successfully authenticated

--- my-mean/src/app/auth/auth.service.ts
+++ my-mean/src/app/auth/auth.service.ts
@@ -42,9 +42,7 @@ export class AuthService {
       this.token = token;
       if (token) {
         const expiresInDuration = response.expiresIn;
-        this.tokenTimer = setTimeout(() => {
-          this.logout();
-        }, expiresInDuration * 1000)
+        this.setAuthTimer(expiresInDuration);
         this.isAuthenticated = true;
         this.authStatusListener.next(true);
         const now = new Date();
@@ -56,6 +54,21 @@ export class AuthService {
     })
   }
 
+  autoAuthUser() {
+    const authInformation = this.getAuthData();
+    if (!authInformation) {
+      return;
+    }
+    const now = new Date();
+    const expiresIn = authInformation.expirationDate.getTime() - now.getTime();
+    if (expiresIn > 0) {
+      this.token = authInformation.token;
+      this.isAuthenticated = true;
+      this.setAuthTimer(expiresIn / 1000);
+      this.authStatusListener.next(true);
+    }
+  }
+
   logout() {
     this.token = null;
     this.isAuthenticated = false;
@@ -65,6 +78,13 @@ export class AuthService {
     this.router.navigate(['/']);
   }
 
+  private setAuthTimer(duration: number) {
+    console.log("Setting Timer: " + duration);
+    this.tokenTimer = setTimeout(() => {
+      this.logout();
+    }, duration * 1000)
+  }
+
   private saveAuthData(token: string, expirationDate: Date ) {
     localStorage.setItem('token', token);
     localStorage.setItem('expiration', expirationDate.toISOString());
@@ -72,6 +92,18 @@ export class AuthService {
   private clearAuthData() {
     localStorage.removeItem("token");
     localStorage.removeItem("expiration");
+  }
+
+  private getAuthData() {
+    const token = localStorage.getItem("token");
+    const expirationDate = localStorage.getItem("expiration");
+    if (!token || !expirationDate) {
+      return;
+    }
+    return {
+      token: token,
+      expirationDate: new Date(expirationDate)
+    }
   }
 }
 
