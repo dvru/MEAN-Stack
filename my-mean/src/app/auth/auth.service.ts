e061e4f3fef05f94d176f142af37435d31944c83
dvru <damvaru4@gmail.com>
2020-05-19 15:41:33 -0500

    Authorization

--- my-mean/src/app/auth/auth.service.ts
+++ my-mean/src/app/auth/auth.service.ts
@@ -23,6 +23,10 @@ export class AuthService {
     return this.isAuthenticated;
   }
 
+  getUserId(){
+    return this.userId;
+  }
+
   getAuthStatusListener(){
     return this.authStatusListener.asObservable();
   }
@@ -50,7 +54,7 @@ export class AuthService {
         const now = new Date();
         const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
         console.log(expirationDate);
-        this.saveAuthData(token, expirationDate)
+        this.saveAuthData(token, expirationDate, this.userId)
         this.router.navigate(['/']);
       }
     })
@@ -66,6 +70,7 @@ export class AuthService {
     if (expiresIn > 0) {
       this.token = authInformation.token;
       this.isAuthenticated = true;
+      this.userId = authInformation.userId;
       this.setAuthTimer(expiresIn / 1000);
       this.authStatusListener.next(true);
     }
@@ -75,6 +80,7 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    this.userId = null;
     clearTimeout(this.tokenTimer);
     this.clearAuthData();
     this.router.navigate(['/']);
@@ -87,24 +93,28 @@ export class AuthService {
     }, duration * 1000)
   }
 
-  private saveAuthData(token: string, expirationDate: Date ) {
+  private saveAuthData(token: string, expirationDate: Date, userId: string ) {
     localStorage.setItem('token', token);
     localStorage.setItem('expiration', expirationDate.toISOString());
+    localStorage.setItem("userId", userId);
   }
   private clearAuthData() {
     localStorage.removeItem("token");
     localStorage.removeItem("expiration");
+    localStorage.removeItem("userId");
   }
 
   private getAuthData() {
     const token = localStorage.getItem("token");
     const expirationDate = localStorage.getItem("expiration");
+    const userId = localStorage.getItem("userId")
     if (!token || !expirationDate) {
       return;
     }
     return {
       token: token,
-      expirationDate: new Date(expirationDate)
+      expirationDate: new Date(expirationDate),
+      userId: userId
     }
   }
 }
