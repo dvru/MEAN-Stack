c60ca5af57da86105b343623d8824f898e7dcbc3
dvru <damvaru4@gmail.com>
2020-05-19 21:40:34 -0500

    cleaned up routes

--- my-mean/src/app/app-routing.module.ts
+++ my-mean/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, Router } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { AuthGuard } from './auth/auth.guard';
@@ -8,7 +8,7 @@ const routes: Routes = [
     { path: '', component: PostListComponent },
     { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard] },
     { path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard] },
-    { path: "auth", loadChildren: "auth/auth.module#AuthModule" }
+    { path: "auth", loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)}
 ];
 
 @NgModule({
