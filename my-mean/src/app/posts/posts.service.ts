84b16c23be83a1903780493f14a79d00d98f0477
dvru <damvaru4@gmail.com>
2020-05-19 21:49:16 -0500

    cleaned up routes

--- my-mean/src/app/posts/posts.service.ts
+++ my-mean/src/app/posts/posts.service.ts
@@ -6,7 +6,7 @@ import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { Form } from '@angular/forms';
 
-
+const BACKEND_URL= "http://localhost:3000/api/posts";
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -19,7 +19,7 @@ export class PostsService {
     const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
     this.http
     .get<{message: string, posts: any, maxPosts: number }>(
-      'http://localhost:3000/api/posts' + queryParams
+      BACKEND_URL + queryParams
       )
       .pipe(map((postData) => {
         return {posts: postData.posts.map(post => {
@@ -47,12 +47,12 @@ export class PostsService {
 
   getPost(id: string) {
     return this.http.get<{
-      _id: string, 
-      title: string, 
-      content: string, 
+      _id: string,
+      title: string,
+      content: string,
       imagePath: string;
       creator: string;
-    }>("http://localhost:3000/api/posts/" + id
+    }>(BACKEND_URL + id
       );
   }
 
@@ -63,7 +63,7 @@ export class PostsService {
     postData.append("image", image, title);
     this.http
     .post<{ message: string, post: Post }>(
-      'http://localhost:3000/api/posts',
+      BACKEND_URL,
       postData
       )
       .subscribe((responseData) => {
@@ -89,14 +89,14 @@ export class PostsService {
         };
       }
 
-      this.http.put('http://localhost:3000/api/posts/' + id, postData)
+      this.http.put(BACKEND_URL + id, postData)
       .subscribe(response => {
         this.router.navigate(["/"]);
       });
     }
 
     deletePosts(postId: string) {
-      return this.http.delete("http://localhost:3000/api/posts/" + postId);
+      return this.http.delete(BACKEND_URL + postId);
     }
 }
 
